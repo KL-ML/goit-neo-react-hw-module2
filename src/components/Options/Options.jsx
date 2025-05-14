965ea2effc0944fa7e67b9f7e4d8b8761cf05e5a
KL-ML <knataly0513@gmail.com>
2025-05-14 15:35:46 +0300

    Create Options.jsx

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,16 @@
+import Button from "../Button/Button";
+import css from "./Options.module.css"
+
+export default function Options({ buttons, onUpdate, total, reset }) {
+
+  return (
+    <>
+      <div className={css.options}>
+        {Object.keys(buttons).map(btn => (
+          <Button key={btn} text={btn} handleClick={onUpdate} variant="filled" />
+        ))}
+        {total !== 0 && <Button text="reset" handleClick={reset} variant="outlined" />}
+      </div>
+    </>
+  );
+}
