86d8a76f50bc6df94e215c85b92e87626dfc81af
KL-ML <knataly0513@gmail.com>
2025-05-14 15:35:33 +0300

    Create Section.jsx

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import css from './Section.module.css';
+
+export default function Section({ children}) {
+  return (
+    <div className={css.section}>
+      {children}
+    </div>
+  );
+}
+
+Section.PropTypes = {
+  children: PropTypes.any,
+};
