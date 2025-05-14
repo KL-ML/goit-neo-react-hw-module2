c5ae4b3a864126e96976e6890eb02f95e577ddc6
KL-ML <knataly0513@gmail.com>
2025-05-14 16:10:06 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,12 +1,8 @@
 import PropTypes from 'prop-types';
 import css from './Section.module.css';
 
-export default function Section({ children}) {
-  return (
-    <div className={css.section}>
-      {children}
-    </div>
-  );
+export default function Section({ children }) {
+  return <div className={css.section}>{children}</div>;
 }
 
 Section.PropTypes = {
